ce191d8c1758625ea6ae8b20b15766c943783dca
corradocoppo <corradocoppolino@gmail.com>
2021-05-26 16:31:30 +0200

    problema axios

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     
-    <header-main />
+    <header-main @src="searchFilms" />
 
     <main-content />
 
@@ -12,6 +12,7 @@
 
 import HeaderMain from './components/HeaderMain.vue'
 import MainContent from './components/MainContent.vue'
+import axios from 'axios'
 
 export default {
 
@@ -20,6 +21,63 @@ export default {
   components: {
     HeaderMain,
     MainContent
+  },
+
+  data(){
+
+  return{
+    apiURL: "https://api.themoviedb.org/3/search/movie",
+    apiKey: "b6ecad6b36ae1c5ecd7e97efa9a42ecf",
+    query: "matrix",
+    films: [],
+    textFilm: ""
+    }
+  
+  },
+
+  methods: {
+
+    callAxios(){
+
+      axios.get(this.apiURL,{
+            params:{
+                api_key: this.apiKey,
+                query: this.query,
+                language: "it-IT"
+            }
+        })
+        .then(res => {
+            /* console.log(res.data.results); */
+            this.films = res.data.results;
+            console.log(this.films)
+        })
+
+    },
+
+    searchFilms(text){
+
+      this.query= text;
+      this.callAxios();
+    
+    }
+
+  },
+
+  beforeCreate() {
+
+    axios.get(this.apiURL,{
+      params:{
+        api_key: this.apiKey,
+        query: this.query,
+        language: "it-IT"
+        }
+    })
+      .then(res => {
+          /* console.log(res.data.results); */
+          this.films = res.data.results;
+          console.log(this.films)
+      })
+
   }
 
 }
--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -2,7 +2,14 @@
   
     <main>
 
-
+        <h1>Pokémon Detective Pikachu</h1>
+        <h1>Pokémon Detective Pikachu</h1>
+        <h1>Pokémon Detective Pikachu</h1>
+        <h1>Pokémon Detective Pikachu</h1>
+        <h1>Pokémon Detective Pikachu</h1>
+        <h1>Pokémon Detective Pikachu</h1>
+        <h1>Pokémon Detective Pikachu</h1>
+        <h1>Pokémon Detective Pikachu</h1>
 
     </main>
 
@@ -10,38 +17,10 @@
 
 <script>
 
-import axios from 'axios';
-
 export default {
     
     name: 'MainContent',
-
-    data(){
-        return{
-            apiURL: "https://api.themoviedb.org/3/search/movie",
-            apiKey: "b6ecad6b36ae1c5ecd7e97efa9a42ecf",
-            query: "pokemon",
-            films: []
-        }
-    },
-
-    created(){
-
-        axios.get(this.apiURL,{
-            params:{
-                api_key: this.apiKey,
-                query: this.query,
-                language: "it-IT"
-            }
-        })
-        .then(res => {
-            /* console.log(res.data.results); */
-            this.films = res.data.results;
-            console.log(this.films)
-        })
-
-    }
-
+    
 }
 </script>
 
